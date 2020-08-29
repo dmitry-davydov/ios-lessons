962e9cdc2f1b3e8e5ac2f748531e80d23ace30c8
Dmitry Davydov <davidov.dima@gmail.com>
2020-08-29 16:07:17 +0300

    l2

---
+++ l2/2l_ДавыдовДмитрий.playground/Contents.swift
@@ -0,0 +1,64 @@
+import UIKit
+//
+// 1. Написать функцию, которая определяет, четное число или нет.
+func isEven(_ number: Int) -> Bool {
+    return number % 2 == 0
+}
+
+// 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func isDevisibleBy3(_ number: Int) -> Bool {
+    return number % 3 == 0
+}
+
+// 3. Создать возрастающий массив из 100 чисел.
+var arr = [Int](stride(from: 1, to: 101, by: 1))
+
+// 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+arr = arr.filter({(num) -> Bool in
+    return !isEven(num) && !isDevisibleBy3(num)
+})
+
+// 5. * Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 100 элементов.
+func fib(_ num: Int) -> UInt64 {
+    if num < 2 {
+        return 1
+    }
+    
+    return fib(num - 1) + fib(num - 2)
+}
+
+var fibbArr: [UInt64] = []
+
+for num in 1...100 {
+    let f = fib(num)
+    print(f)
+    fibbArr.append(f)
+}
+
+/*
+ 6. * Заполнить массив из 100 элементов различными простыми числами. Натуральное число, большее единицы, называется простым, если оно делится только на себя и на единицу. Для нахождения всех простых чисел не больше заданного числа n, следуя методу Эратосфена, нужно выполнить следующие шаги:
+*/
+func naturalNumbers(_ max: Int) -> [Int] {
+    // a. Выписать подряд все целые числа от двух до n (2, 3, 4, ..., n).
+    var data: [Int] = Array(2...max)
+    // b. Пусть переменная p изначально равна двум — первому простому числу.
+    var testValue = 2
+    
+    while testValue <= max {
+        data.removeAll(where: {(number) -> Bool in
+            // удаляем все элементы, если число больше чем testValue и кратно testValue
+            return number >= testValue * testValue && number % testValue == 0
+        })
+        
+        let val = data.first(where: {(number) -> Bool in
+            return number > testValue
+        })
+        
+        guard val != nil else {return data}
+        
+        testValue = val!
+    }
+    
+    
+    return data
+}
